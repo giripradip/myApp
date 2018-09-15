b650253ce0631c8893a9e237e4a879bf56b4b5ce
Pradip Giri <mepradipjee@gmail.com>
2018-09-15 19:19:45 +0200

    Adds pipe for user filtering

---
+++ src/pipes/pipes.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ContactPipe } from './contact/contact';
+@NgModule({
+    declarations: [ContactPipe],
+    imports: [],
+    exports: [ContactPipe]
+})
+export class PipesModule { }
