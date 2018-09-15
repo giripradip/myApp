70dcc574b22a4e54dfa270d6d2622ba3cb4a2f83
Pradip Giri <mepradipjee@gmail.com>
2018-09-15 19:17:30 +0200

    Adds create nick name page with implementation

--- src/pages/custom-name/custom-name.ts
+++ src/pages/custom-name/custom-name.ts
@@ -1,24 +1,44 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { NavController, NavParams } from 'ionic-angular';
+import * as _ from "lodash";
 
-/**
- * Generated class for the CustomNamePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { HomePage } from '../home/home';
 
 @Component({
-  selector: 'page-custom-name',
-  templateUrl: 'custom-name.html',
+    selector: 'page-custom-name',
+    templateUrl: 'custom-name.html',
 })
 export class CustomNamePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    public customName: FormGroup;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CustomNamePage');
-  }
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        private formBuilder: FormBuilder) {
+
+        this.customName = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+        });
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad CustomNamePage');
+    }
+
+    createNickName() {
+        let nickName = this.generateNickName(this.customName.value).toUpperCase();
+        this.customName.reset()
+        this.navCtrl.setRoot(HomePage, { nickName: nickName });
+    }
+
+    private generateNickName(name: any): string {
+        if (!_.isEmpty(name)) {
+            return name.firstName.charAt(0) + name.lastName.charAt(0);
+        }
+        return;
+    }
 
 }
