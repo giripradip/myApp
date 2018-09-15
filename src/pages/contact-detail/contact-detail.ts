f1e0a225c1243c0d667be656d33ba1102db05ad4
Pradip Giri <mepradipjee@gmail.com>
2018-09-15 19:16:33 +0200

    Adds contact details page with logic and implementation

--- src/pages/contact-detail/contact-detail.ts
+++ src/pages/contact-detail/contact-detail.ts
@@ -1,24 +1,62 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Events } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular'; import { HttpErrorResponse } from '@angular/common/http';
 
-/**
- * Generated class for the ContactDetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { User } from '../../helper/user';
+import { MyAppApiProvider } from '../../providers/my-app-api/my-app-api';
+import { CreateContactPage } from '../create-contact/create-contact';
+import { Constant } from '../../helper/constant';
+
 
 @Component({
-  selector: 'page-contact-detail',
-  templateUrl: 'contact-detail.html',
+    selector: 'page-contact-detail',
+    templateUrl: 'contact-detail.html',
 })
 export class ContactDetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    public contact: User;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ContactDetailPage');
-  }
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public events: Events,
+        public loadingController: LoadingController,
+        private apiProvider: MyAppApiProvider) {
+        this.contact = this.navParams.data;
+    }
+
+    ionViewDidLoad() {
+
+    }
+
+    ionViewWillEnter() {
+        let contact = this.navParams.get(Constant.CONTACT);
+        if (contact) {
+            let name = contact.name.split(" ");
+            this.contact.first_name = name[0];
+            this.contact.last_name = name[1];
+        }
+    }
+
+    deleteContact(event: any, contact: User) {
+        let loader = this.loadingController.create({
+            content: "Deleting contact..."
+        });
+        loader.present();
+        this.apiProvider.deleteContact(contact.id).subscribe(response => {
+            loader.dismiss();
+            this.events.publish(Constant.CONTACT, contact);
+            this.navCtrl.pop();
+        },
+            (err: HttpErrorResponse) => {
+                console.log(err);
+                loader.dismiss();
+            });
+    }
+
+    updateContact(event: any, contact: User) {
+        let uContact = { contact: contact };
+        this.navCtrl.push(CreateContactPage, uContact);
+    }
 
 }
