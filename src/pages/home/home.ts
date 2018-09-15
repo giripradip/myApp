f4bc3e954edeb640a2fb3171ca2b95be2185b565
Pradip Giri <mepradipjee@gmail.com>
2018-09-15 19:18:06 +0200

    Adds home page with implementation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,78 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { ContactDetailPage } from '../contact-detail/contact-detail';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
+
+import { TabPage } from '../tab/tab';
+import { MyAppApiProvider } from '../../providers/my-app-api/my-app-api';
+import { User } from '../../helper/user';
+import { LoginPage } from '../login/login';
+import { Constant } from '../../helper/constant';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    public user = <User>{};
+    public nickName: string;
+    public isLoggedIn: boolean = false;
 
-  }
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public loadingController: LoadingController,
+        private apiProvider: MyAppApiProvider) { }
 
-  goToContactDetail() {
-      this.navCtrl.push(ContactDetailPage);
-  }
+    ionViewDidLoad() {
+        this.apiProvider.isAuthenticated().then(
+            (val: string | null) => {
+                if (val !== null) {
+                    this.getUser();
+                }
+            });
+    }
 
+    ionViewCanEnter() {
+        this.apiProvider.isAuthenticated().then(
+            (val: string | null) => {
+                if (val !== null) {
+                    this.isLoggedIn = true;
+                    return true;
+                }
+                this.isLoggedIn = false;
+                return false;
+            });
+    }
+
+    ionViewWillEnter() {
+        let nickName = this.navParams.get(Constant.NICKNAME);
+        if (nickName) {
+            this.nickName = nickName;
+        }
+    }
+
+    goToLoginPage() {
+        this.navCtrl.setRoot(LoginPage);
+    }
+
+    goToContactsPage() {
+        this.navCtrl.setRoot(TabPage);
+    }
+
+    getUser() {
+        let loader = this.loadingController.create({
+            content: "Loading..."
+        });
+        loader.present();
+        this.apiProvider.getUser().subscribe(user => {
+            this.user = user;
+            loader.dismiss();
+        },
+            (err: HttpErrorResponse) => {
+                console.log(err);
+                loader.dismiss();
+            });
+    }
 }
+
