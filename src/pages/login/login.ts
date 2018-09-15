dbdeda8af5018941c419bde2918f7716a325e969
Pradip Giri <mepradipjee@gmail.com>
2018-09-15 19:18:25 +0200

    Adds login page with implementation

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,24 +1,51 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NavController, NavParams, LoadingController, Events } from 'ionic-angular';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { AuthenticationCredentials, AuthenticatedCredentials } from '../../helper/authentication';
+import { MyAppApiProvider } from '../../providers/my-app-api/my-app-api';
+import { HomePage } from '../home/home';
+import { Constant } from '../../helper/constant';
 
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+    selector: 'page-login',
+    templateUrl: 'login.html',
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    authenticationCredential: AuthenticationCredentials;
+    authenticatedCredentials: AuthenticatedCredentials;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public events: Events,
+        public loadingController: LoadingController,
+        private apiProvider: MyAppApiProvider) {
+        this.authenticationCredential = new AuthenticationCredentials();
+    }
+
+    ionViewDidLoad() {
+        //this.apiProvider.logout();
+    }
+
+    // method to call when user clicks login buttom
+    login() {
+        let loader = this.loadingController.create({
+            content: "Logging in..."
+        });
+        loader.present();
+        this.apiProvider.login(this.authenticationCredential).subscribe(authenticatedCredentials => {
+            this.apiProvider.setSession(authenticatedCredentials).then((result: any)=> {
+                this.events.publish(Constant.USER_EVENT);
+                loader.dismiss();
+                this.navCtrl.setRoot(HomePage);
+            });
+        },
+            (err: HttpErrorResponse) => {
+                console.log(err);
+                loader.dismiss();
+            });
+    }
 
 }
