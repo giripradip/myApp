c56e68bbfdad21fed6068b58eba1e9cf364df4fd
Pradip Giri <mepradipjee@gmail.com>
2018-09-15 19:16:57 +0200

    Adds create contact page with logic and implementation

---
+++ src/pages/create-contact/create-contact.ts
@@ -0,0 +1,104 @@
+import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+
+import { CreateContact } from '../../helper/create-contact';
+import { MyAppApiProvider } from '../../providers/my-app-api/my-app-api';
+import { TabPage } from '../tab/tab';
+import { User } from '../../helper/user';
+import { Constant } from '../../helper/constant';
+
+@IonicPage()
+@Component({
+    selector: 'page-create-contact',
+    templateUrl: 'create-contact.html',
+})
+export class CreateContactPage {
+
+    public createContactForm: FormGroup;
+    public btnLabel: string;
+    private contact: User;
+
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public loadingController: LoadingController,
+        private apiProvider: MyAppApiProvider,
+        private formBuilder: FormBuilder) {
+        this.contact = this.navParams.get(Constant.CONTACT);
+        this.initializeCreateContactForm();
+    }
+
+    ionViewDidLoad() {
+
+    }
+
+    createContact() {
+        if (!this.contact) {
+            this.createNewContact();
+            return;
+        }
+        this.updateContact();
+    }
+
+    private createNewContact() {
+
+        let loader = this.loadingController.create({
+            content: "Creating contact..."
+        });
+        loader.present();
+        this.apiProvider.createContact(this.getNewContactObj()).subscribe(newC => {
+            loader.dismiss();
+            this.createContactForm.reset()
+            this.navCtrl.setRoot(TabPage, newC);
+        },
+            (err: HttpErrorResponse) => {
+                console.log(err);
+                loader.dismiss();
+            });
+    }
+
+    private updateContact() {
+        let loader = this.loadingController.create({
+            content: "Updating contact..."
+        });
+        loader.present();
+        this.apiProvider.updateContact(this.getNewContactObj()).subscribe(newC => {
+            loader.dismiss();
+            this.createContactForm.reset()
+            this.navCtrl.getPrevious().data.contact = newC;
+            this.navCtrl.pop();
+        },
+            (err: HttpErrorResponse) => {
+                console.log(err);
+                loader.dismiss();
+            });
+    }
+
+    private getNewContactObj(): CreateContact {
+        let newContact = new CreateContact();
+        newContact.name = this.createContactForm.value.firstName + " " + this.createContactForm.value.lastName;
+        newContact.job = this.createContactForm.value.job
+        return newContact;
+    }
+
+    private initializeCreateContactForm() {
+        if (!this.contact) {
+            this.btnLabel = "Create Contact";
+            this.createContactForm = this.formBuilder.group({
+                firstName: ['', Validators.required],
+                lastName: ['', Validators.required],
+                job: ['', Validators.required],
+            });
+            return;
+        }
+        this.btnLabel = "Update Contact";
+        this.createContactForm = this.formBuilder.group({
+            firstName: [this.contact.first_name, Validators.required],
+            lastName: [this.contact.last_name, Validators.required],
+            job: ['', Validators.required],
+        });
+    }
+
+}
